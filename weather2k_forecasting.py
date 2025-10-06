7a6adff185936bcb6ade6014995574b98d0fcdad
Sai Vamsi Alisetti <vamsi@169-231-132-49.wireless.ucsb.edu>
2025-10-06 15:02:49 -0700

    chaning gru hidden dim

--- weather2k_forecasting.py
+++ weather2k_forecasting.py
@@ -482,8 +482,8 @@ class Hyperparameters:
         self.batch_size = 8
         self.n_feat = 1  # Number of features for each node (adjust according to your dataset)
         self.n_hidden = 64
-        self.gru_hidden = 64
-        self.n_heads = 2
+        self.gru_hidden = 128
+        self.n_heads = 4
         self.dropout = 0.6
         self.alpha = 0.2
         self.output_dim = 12  # Traffic flow prediction output (1 for single step, adjust as needed)
